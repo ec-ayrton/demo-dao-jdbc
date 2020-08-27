5d636b681dae61fd4e1a9f42037abe2cadc563cc
ec-ayrton <ec.ayrton@gmail.com>
2020-08-27 12:28:46 -0300

    implementando o delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+ 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("=== TEST 1: seller findById =====");
 
@@ -44,5 +47,12 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 
+		System.out.println("\n=== TEST 6: seller delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 }
