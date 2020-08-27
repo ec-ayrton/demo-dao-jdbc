8688d208e40c7329cae0f5e1a65218b5de9be685
ec-ayrton <ec.ayrton@gmail.com>
2020-08-27 08:52:56 -0300

    reusando instanciacao

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -106,4 +106,6 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
+
+	
 }
